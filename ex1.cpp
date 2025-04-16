9a50873e496c8a628e1a676567cb69cc1e26a639
anajuliatemoteo <ana.temoteo@aluno.ifsp.edu.br>
2025-04-16 20:37:50 -0300

    Add files via upload

---
+++ ex1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main () 
+{
+    int base;
+	int altura;
+	int area;
+	
+	cout << "informe a base do retângulo: ";
+	cin >> base;
+	
+	cout << "informe a altura do retângulo: ";
+	cin >> altura;
+	
+	area = base * altura;
+	
+	cout << "A area do retangulo es " << area << endl;
+}
---
+++ ex2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main () 
+{
+	int aresta;
+	int area;
+	
+	  cout << "Informe a aresta do quadrado: ";
+	  cin >> aresta;
+	  
+	  area = aresta * aresta;
+	  
+	  cout << " A area do quadrado eh " << area << endl;
+	  
+}
